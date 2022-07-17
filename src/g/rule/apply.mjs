96b692c0d82880585a7549fb7b0e6ba2f2941137
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:45:04 -0400

    main file_js_auto g_rule_apply

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -1,3 +1,5 @@
+import {m_js_number_is} from "./../../../node_modules/mykro/src/m/js/number/is.mjs";
+import {m_js_string_is} from "./../../../node_modules/mykro/src/m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_rule_apply(input, rule, index) {
   await m_js_arguments_assert(m_js_string_is, g_rule_is, m_js_number_is)(arguments);
