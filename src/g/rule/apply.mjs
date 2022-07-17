16be8f167708e05f284f24a9ba2940820fcabcf5
JaredMathis <jared.mathis@native.consulting>
2022-07-17 13:18:11 -0400

    main file_js_auto g_rule_apply

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -1,4 +1,6 @@
+import {g_rule_is} from "./is.mjs";
+import {list_is} from "./../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_rule_apply() {
-  await m_js_arguments_assert()(arguments);
+export async function g_rule_apply(input, rule) {
+  await m_js_arguments_assert(list_is, g_rule_is)(arguments);
 }
