d139b0079261a0203dd760eb765188522111d7db
JaredMathis <jared.mathis@native.consulting>
2022-07-17 18:10:30 -0400

    main file_js_auto g_explore

--- src/g/explore.mjs
+++ src/g/explore.mjs
@@ -1,4 +1,15 @@
+import {g_rule_apply} from "./rule/apply.mjs";
+import {m_js_for_each} from "./../../node_modules/mykro/src/m/js/for/each.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_explore() {
-  await m_js_arguments_assert()(arguments);
+async function g_explore(start, rules, depth, for_each_depth_0) {
+  if (depth === 0) {
+    await for_each_depth_0(start);
+    return;
+  }
+  await m_js_for_each(rules, async rule => {
+    let applies = await g_rule_apply(start, rule);
+    await m_js_for_each(applies, async applied => {
+      await g_explore(applied, rules, depth - 1, for_each_depth_0);
+    });
+  });
 }
