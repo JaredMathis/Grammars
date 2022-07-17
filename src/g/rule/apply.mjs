da0dd21d5c8d376223782aa15bcc5963b551420f
JaredMathis <jared.mathis@native.consulting>
2022-07-17 13:22:15 -0400

    main file_js_auto g_rule_apply

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -5,7 +5,7 @@ import {list_range} from "./../../../node_modules/mykro/src/list/range.mjs";
 import {g_rule_is} from "./is.mjs";
 import {list_is} from "./../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import {m_js_for_each} from "mykro/src/m/js/for/each.mjs";
+import {m_js_for_each} from "./../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {g_rule_apply_at} from "./apply/at.mjs";
 export async function g_rule_apply(input, rule) {
   await m_js_arguments_assert(list_is, g_rule_is)(arguments);
