29310a328b7b792188af647d387970dcb3e43b00
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:54:47 -0400

    main file_js_auto g_rule_apply

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -1,3 +1,5 @@
+import {list_join} from "./../../../node_modules/mykro/src/list/join.mjs";
+import {m_js_string_size} from "./../../../node_modules/mykro/src/m/js/string/size.mjs";
 import {list_take} from "./../../../node_modules/mykro/src/list/take.mjs";
 import {list_starting_at} from "./../../../node_modules/mykro/src/list/starting/at.mjs";
 import {m_js_string_listify} from "./../../../node_modules/mykro/src/m/js/string/listify.mjs";
@@ -7,12 +9,17 @@ import {m_js_string_is} from "./../../../node_modules/mykro/src/m/js/string/is.m
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {m_js_equals} from "./../../../node_modules/mykro/src/m/js/equals.mjs";
 import {m_js_assert} from "mykro/src/m/js/assert.mjs";
+import {m_js_string_to_list} from "mykro/src/m/js/string/to/list.mjs";
 export async function g_rule_apply(input, rule, index) {
   await m_js_arguments_assert(m_js_string_is, g_rule_is, m_js_number_is)(arguments);
-  let {left} = rule;
+  let {left: rule_left, right: rule_right} = rule;
   await m_js_string_listify(async l => {
     let offset = await list_starting_at(l, index);
-    let replaced = await list_take(offset, left.length);
-    await m_js_assert(m_js_equals)(replaced, left);
+    let replaced = await list_take(offset, rule_left.length);
+    await m_js_assert(m_js_equals)(replaced, rule_left);
+    let replacement_left = await list_take(l, index);
+    let replacement_right = await list_starting_at(l, index + await m_js_string_size(rule_left));
+    let replacement = await m_js_string_to_list(rule_right);
+    return await list_join([replacement_left, replacement, replacement_right]);
   });
 }
