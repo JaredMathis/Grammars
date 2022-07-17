b1be913dabed6f897ab6164c14e3d054628f7997
JaredMathis <jared.mathis@native.consulting>
2022-07-17 13:02:32 -0400

    main noop unused functions

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -2,17 +2,12 @@ import {list_size} from "./../../../node_modules/mykro/src/list/size.mjs";
 import {list_is} from "./../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_equals_json} from "./../../../node_modules/mykro/src/m/js/equals/json.mjs";
 import {list_join} from "./../../../node_modules/mykro/src/list/join.mjs";
-import {m_js_string_size} from "./../../../node_modules/mykro/src/m/js/string/size.mjs";
 import {list_take} from "./../../../node_modules/mykro/src/list/take.mjs";
 import {list_starting_at} from "./../../../node_modules/mykro/src/list/starting/at.mjs";
-import {m_js_string_listify} from "./../../../node_modules/mykro/src/m/js/string/listify.mjs";
 import {g_rule_is} from "./is.mjs";
 import {m_js_number_is} from "./../../../node_modules/mykro/src/m/js/number/is.mjs";
-import {m_js_string_is} from "./../../../node_modules/mykro/src/m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import {m_js_equals} from "./../../../node_modules/mykro/src/m/js/equals.mjs";
 import {m_js_assert} from "./../../../node_modules/mykro/src/m/js/assert.mjs";
-import {m_js_string_to_list} from "./../../../node_modules/mykro/src/m/js/string/to/list.mjs";
 export async function g_rule_apply(input, rule, index) {
   await m_js_arguments_assert(list_is, g_rule_is, m_js_number_is)(arguments);
   let {left: rule_left, right: rule_right} = rule;
