ebd13381ca40dce2c3ee7d784acb82d7095eddea
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:45:28 -0400

    main file_js_argument_add argadd g_rule_is rule m_js_defined_is

--- src/g/rule/is.mjs
+++ src/g/rule/is.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_rule_is() {
-  await m_js_arguments_assert()(arguments);
+export async function g_rule_is(rule) {
+  await m_js_arguments_assert(m_js_defined_is)(arguments);
 }
