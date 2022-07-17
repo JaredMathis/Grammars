13a6a5c80b281e1612a1a480a9e187b06708ec97
JaredMathis <jared.mathis@native.consulting>
2022-07-17 18:54:59 -0400

    main sandbox sandbox

--- src/g/explore.mjs
+++ src/g/explore.mjs
@@ -1,4 +1,3 @@
-import {g_rule_apply} from "./rule/apply.mjs";
 import {g_rule_apply_get} from "./rule/apply/get.mjs";
 import {m_js_for_each} from "./../../node_modules/mykro/src/m/js/for/each.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
@@ -8,7 +7,8 @@ export async function g_explore(start, rules, depth, for_each_depth_0) {
     return;
   }
   await m_js_for_each(rules, async rule => {
-    await g_rule_apply(start, rule, async applied => {
+    let applies = await g_rule_apply_get(start, rule);
+    await m_js_for_each(applies, async applied => {
       await g_explore(applied, rules, depth - 1, for_each_depth_0);
     });
   });
