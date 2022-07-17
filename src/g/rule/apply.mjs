d8ab6672ce9e62f10fd13e007138f29b3d0b5df5
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:48:59 -0400

    main noop progress

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -4,4 +4,11 @@ import {m_js_string_is} from "./../../../node_modules/mykro/src/m/js/string/is.m
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_rule_apply(input, rule, index) {
   await m_js_arguments_assert(m_js_string_is, g_rule_is, m_js_number_is)(arguments);
+
+  let {left} = rule;
+  m_js_string_listify(l => {
+    let offset = list_starting_at(l, index);
+    list_take(offset, left.length);
+  })
+  rule.left
 }
