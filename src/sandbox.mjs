83e4390ef355966042135030923a4a755bae8ff1
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:43:27 -0400

    main noop progress

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,4 +1,10 @@
 import {m_js_arguments_assert} from "./../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function sandbox() {
   await m_js_arguments_assert()(arguments);
+  let rule = {
+    left: ['a'],
+    right: ['aa'],
+  };
+  let input = 'abb';
+  let result = await g_rule_apply(input, rule, 0);
 }
