1b9f34c348aca25f933171cc80e7cf51f0f51de5
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:49:26 -0400

    main file_js_auto g_rule_apply

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -1,13 +1,15 @@
+import {list_take} from "./../../../node_modules/mykro/src/list/take.mjs";
+import {list_starting_at} from "./../../../node_modules/mykro/src/list/starting/at.mjs";
+import {m_js_string_listify} from "./../../../node_modules/mykro/src/m/js/string/listify.mjs";
 import {g_rule_is} from "./is.mjs";
 import {m_js_number_is} from "./../../../node_modules/mykro/src/m/js/number/is.mjs";
 import {m_js_string_is} from "./../../../node_modules/mykro/src/m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_rule_apply(input, rule, index) {
   await m_js_arguments_assert(m_js_string_is, g_rule_is, m_js_number_is)(arguments);
-
   let {left} = rule;
   await m_js_string_listify(async l => {
     let offset = await list_starting_at(l, index);
     await list_take(offset, left.length);
-  })
+  });
 }
