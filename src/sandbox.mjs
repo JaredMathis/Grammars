3459f5eb1da573f2088f1a07b044c89c399683fe
JaredMathis <jared.mathis@native.consulting>
2022-07-17 17:56:13 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,27 +1,28 @@
+import {list_remove} from "./../node_modules/mykro/src/list/remove.mjs";
+import {list_add} from "./../node_modules/mykro/src/list/add.mjs";
 import {g_rule_apply_at} from "./g/rule/apply/at.mjs";
 import {m_js_arguments_assert} from "./../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import { g_rule_apply } from "./g/rule/apply.mjs";
-import { m_js_for_each } from "mykro/src/m/js/for/each.mjs";
+import {g_rule_apply} from "./g/rule/apply.mjs";
+import {m_js_for_each} from "mykro/src/m/js/for/each.mjs";
 export async function sandbox() {
-  let rules = [
-    {
-      left: ["a"],
-      right: ["a", "a"]
-    },
-    {
-      left: ["a"],
-      right: ["1"],
-    },
-    {
-      left: ["a"],
-      right: ["0"],
-    },
-  ];
-
+  let rules = [{
+    left: ["a"],
+    right: ["a", "a"]
+  }, {
+    left: ["a"],
+    right: ["1"]
+  }, {
+    left: ["a"],
+    right: ["0"]
+  }];
+  let depth = 0;
   let start = ["a"];
-
   await m_js_for_each(rules, async rule => {
     let applies = await g_rule_apply(start, rule);
-    console.log(applies)
-  })
+    await m_js_for_each(applies, async applied => {
+      await list_add(rules, rule);
+      console.log(rules);
+      await list_remove(rules, rule);
+    });
+  });
 }
