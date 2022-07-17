4ee62fdf193819f38f65fb15403e0899a9d61e2e
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:44:25 -0400

    main file_js_argument_add argadd g_rule_apply input m_js_string_is

--- src/g/rule/apply.mjs
+++ src/g/rule/apply.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_rule_apply() {
-  await m_js_arguments_assert()(arguments);
+export async function g_rule_apply(input) {
+  await m_js_arguments_assert(m_js_string_is)(arguments);
 }
