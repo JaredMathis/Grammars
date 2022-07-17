f1709dedc1b3053854373a111b5e958061cbcc8e
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:55:23 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,10 +1,12 @@
+import {g_rule_apply} from "./g/rule/apply.mjs";
 import {m_js_arguments_assert} from "./../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function sandbox() {
   await m_js_arguments_assert()(arguments);
   let rule = {
-    left: ['a'],
-    right: ['aa'],
+    left: ["a"],
+    right: ["aa"]
   };
-  let input = 'abb';
+  let input = "abb";
   let result = await g_rule_apply(input, rule, 0);
+  console.log(result);
 }
