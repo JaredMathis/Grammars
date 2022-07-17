79e028ca4fa69c3a686ca810a0579311f6df4e11
JaredMathis <jared.mathis@native.consulting>
2022-07-17 12:46:21 -0400

    main file_js_auto g_rule_is

--- src/g/rule/is.mjs
+++ src/g/rule/is.mjs
@@ -1,5 +1,8 @@
+import {m_js_comment} from "./../../../node_modules/mykro/src/m/js/comment.mjs";
 import {m_js_defined_is} from "./../../../node_modules/mykro/src/m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_rule_is(rule) {
   await m_js_arguments_assert(m_js_defined_is)(arguments);
+  await m_js_comment("TODO");
+  return true;
 }
