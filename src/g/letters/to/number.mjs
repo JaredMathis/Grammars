927abe2d2adbdec629030e87fc2b0fced6881273
JaredMathis <jared.mathis@native.consulting>
2022-07-18 00:07:04 -0400

    main constant_alphabet alpha

--- src/g/letters/to/number.mjs
+++ src/g/letters/to/number.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_letters_to_number() {
-  await m_js_arguments_assert()(arguments);
+export async function g_letters_to_number(s) {
+  await m_js_arguments_assert(m_js_string_is)(arguments);
+  
 }
