b8139c5f306bd836bcf9b2eadd6966c9863e7dce
JaredMathis <jared.mathis@native.consulting>
2022-07-18 00:13:59 -0400

    main file_js_auto g_letters_to_number

--- src/g/letters/to/number.mjs
+++ src/g/letters/to/number.mjs
@@ -1,3 +1,5 @@
+import {list_index_last} from "./../../../../node_modules/mykro/src/list/index/last.mjs";
+import {list_take} from "./../../../../node_modules/mykro/src/list/take.mjs";
 import {constant_numbers} from "./../../../../node_modules/mykro/src/constant/numbers.mjs";
 import {m_js_string_to_list} from "./../../../../node_modules/mykro/src/m/js/string/to/list.mjs";
 import {constant_alphabet} from "./../../../../node_modules/mykro/src/constant/alphabet.mjs";
@@ -7,7 +9,9 @@ export async function g_letters_to_number(s) {
   await m_js_arguments_assert(m_js_string_is)(arguments);
   let letters_as_list = await constant_letters_sorted();
   let numbers = await constant_numbers();
-  console.log(numbers);
+  let numbers_as_list = await m_js_string_to_list(numbers);
+  let numbers_without_0 = await list_take(numbers_as_list, await list_index_last(numbers_as_list));
+  console.log(numbers_without_0);
   console.log(letters_as_list);
 }
 async function constant_letters_sorted() {
