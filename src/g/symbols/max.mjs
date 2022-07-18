15b8b8cbe51896930469ff338489faffa2b913fb
JaredMathis <jared.mathis@native.consulting>
2022-07-18 14:47:38 -0400

    main file_js_auto g_symbols_max

--- src/g/symbols/max.mjs
+++ src/g/symbols/max.mjs
@@ -1,4 +1,20 @@
+import {g_letters_to_number} from "./../letters/to/number.mjs";
+import {list_size} from "./../../../node_modules/mykro/src/list/size.mjs";
+import {list_map} from "./../../../node_modules/mykro/src/list/map.mjs";
+import {list_join} from "./../../../node_modules/mykro/src/list/join.mjs";
+import {list_unique} from "./../../../node_modules/mykro/src/list/unique.mjs";
+import {list_is} from "./../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_symbols_max() {
-  await m_js_arguments_assert()(arguments);
+export async function g_symbols_max(rules) {
+  await m_js_arguments_assert(list_is)(arguments);
+  let symbols = await list_unique(await list_join(await list_map(await rules, async rule => await list_join([rule.left, rule.right]))));
+  if (await list_size(symbols) === 0) {
+    symbols = ["a"];
+  }
+  let symbols_mapped = await list_map(symbols, async s => await g_letters_to_number(s));
+  let max = Math.max(...symbols_mapped, 0);
+  return {
+    max,
+    symbols
+  };
 }
