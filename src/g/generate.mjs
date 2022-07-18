8d31d020841ee19d05bbbe5a88997456d4043b55
JaredMathis <jared.mathis@native.consulting>
2022-07-17 23:45:05 -0400

    main file_js_add g_generate_rules

--- src/g/generate.mjs
+++
@@ -1,5 +0,0 @@
-import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_generate(rows) {
-  await m_js_arguments_assert()(arguments);
-  
-}
---
+++ src/g/generate/rules.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+export async function g_generate_rules() {
+  await m_js_arguments_assert()(arguments);
+}
