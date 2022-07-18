1571dbbf679c4d14d6c7c25920beef4e3db3a439
JaredMathis <jared.mathis@native.consulting>
2022-07-17 20:09:09 -0400

    main file_js_add g_generate_side

--- src/g/generate.mjs
+++ src/g/generate.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_generate() {
+export async function g_generate(symbol_count) {
   await m_js_arguments_assert()(arguments);
+
 }
