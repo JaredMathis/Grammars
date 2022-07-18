09ba70f5f53ba6befe773791a66d2ef3c65e84d4
JaredMathis <jared.mathis@native.consulting>
2022-07-18 14:53:42 -0400

    main file_js_auto g_symbols_max

--- src/g/symbols/max.mjs
+++ src/g/symbols/max.mjs
@@ -1,3 +1,4 @@
+import {g_rule_symbols} from "./../rule/symbols.mjs";
 import {g_letters_to_number} from "./../letters/to/number.mjs";
 import {list_size} from "./../../../node_modules/mykro/src/list/size.mjs";
 import {list_map} from "./../../../node_modules/mykro/src/list/map.mjs";
@@ -7,7 +8,7 @@ import {list_is} from "./../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_symbols_max(rules) {
   await m_js_arguments_assert(list_is)(arguments);
-  let symbols = await list_unique(await list_join(await list_map(await rules, async rule => await list_join([rule.left, rule.right]))));
+  let symbols = await list_unique(await list_join(await list_map(await rules, async rule => await g_rule_symbols(rule))));
   if (await list_size(symbols) === 0) {
     symbols = ["a"];
   }
