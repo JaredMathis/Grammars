b47b7a593dc92e267aa9297895a7d15f29c6159d
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:38:38 -0400

    main file_js_auto g_letters_from_number

--- src/g/letters/from/number.mjs
+++ src/g/letters/from/number.mjs
@@ -1,3 +1,4 @@
+import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_string_listify} from "./../../../../node_modules/mykro/src/m/js/string/listify.mjs";
 import {g_letters_number_lookups} from "./../number/lookups.mjs";
 import {m_js_number_is} from "./../../../../node_modules/mykro/src/m/js/number/is.mjs";
@@ -8,8 +9,10 @@ export async function g_letters_from_number(n) {
   await m_js_arguments_assert(m_js_number_is)(arguments);
   let {to_letter} = await g_letters_number_lookups();
   let a = n.toString(await list_size(await constant_alphabet()));
-  let result = await m_js_string_listify(a, b => {
-    return to_letter[b];
+  let result = await m_js_string_listify(s, async l => {
+    await list_map(l, async letter => {
+      return to_letter[letter];
+    });
   });
   return result;
 }
