e0c4d044041ea8bd842d0ff269187f31ba771d3c
JaredMathis <jared.mathis@native.consulting>
2022-07-18 11:04:47 -0400

    main file_js_auto g_generate_rules_depth

--- src/g/generate/rules/depth.mjs
+++ src/g/generate/rules/depth.mjs
@@ -1,4 +1,7 @@
+import {m_js_function_is} from "./../../../../node_modules/mykro/src/m/js/function/is.mjs";
+import {m_js_number_is} from "./../../../../node_modules/mykro/src/m/js/number/is.mjs";
+import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_generate_rules_depth() {
-  await m_js_arguments_assert()(arguments);
+export async function g_generate_rules_depth(rules, depth, for_each_generated) {
+  await m_js_arguments_assert(list_is, m_js_number_is, m_js_function_is)(arguments);
 }
