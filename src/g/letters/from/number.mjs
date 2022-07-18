50ffa1aa28099cf8c174196076d36a1dd2a58ac4
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:27:51 -0400

    main file_js_auto g_letters_to_number

--- src/g/letters/from/number.mjs
+++ src/g/letters/from/number.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_letters_from_number() {
   await m_js_arguments_assert()(arguments);
+  let {to_letter} = await g_letters_number_lookups();
 }
--- src/g/letters/to/number.mjs
+++ src/g/letters/to/number.mjs
@@ -1,3 +1,5 @@
+import {m_js_string_listify} from "./../../../../node_modules/mykro/src/m/js/string/listify.mjs";
+import {g_letters_number_lookups} from "./../number/lookups.mjs";
 import {m_js_string_from_list} from "./../../../../node_modules/mykro/src/m/js/string/from/list.mjs";
 import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_string_to_list} from "./../../../../node_modules/mykro/src/m/js/string/to/list.mjs";
@@ -6,10 +8,8 @@ import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/a
 export async function g_letters_to_number(s) {
   await m_js_arguments_assert(m_js_string_is)(arguments);
   let {from_letter} = await g_letters_number_lookups();
-  let s_as_list = await m_js_string_to_list(s);
-  let mapped = await list_map(s_as_list, letter => {
+  let mapped_as_string = await m_js_string_listify(s, letter => {
     return from_letter[letter];
   });
-  let mapped_as_string = await m_js_string_from_list(mapped);
   return parseInt(mapped_as_string, 26);
 }
