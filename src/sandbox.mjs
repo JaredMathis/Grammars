abc77135fef5487c519e46ed79f092aac5d88326
JaredMathis <jared.mathis@native.consulting>
2022-07-18 10:12:34 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,3 +1,4 @@
+import {m_js_equals_json} from "./../node_modules/mykro/src/m/js/equals/json.mjs";
 import {g_generate_rules} from "./g/generate/rules.mjs";
 import {g_explore} from "./g/explore.mjs";
 import {list_remove} from "./../node_modules/mykro/src/list/remove.mjs";
@@ -6,33 +7,25 @@ import {g_rule_apply_at} from "./g/rule/apply/at.mjs";
 import {m_js_arguments_assert} from "./../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {g_rule_apply_get} from "./g/rule/apply/get.mjs";
 import {m_js_for_each} from "mykro/src/m/js/for/each.mjs";
-import { g_letters_to_number } from "./g/letters/to/number.mjs";
-import { g_letters_from_number } from "./g/letters/from/number.mjs";
-import { list_index_of } from "mykro/src/list/index/of.mjs";
+import {g_letters_to_number} from "./g/letters/to/number.mjs";
+import {g_letters_from_number} from "./g/letters/from/number.mjs";
+import {list_index_of} from "mykro/src/list/index/of.mjs";
 export async function sandbox() {
   await g_generate_rules([{
     left: ["a"],
     right: ["a"]
   }], async rules => {
     await g_generate_rules(rules, async rules => {
-      let examples = [
-        "b",
-        "bb",
-      ]
-      let counter_examples = [
-        "c",
-        "bc",
-        "cb",
-        "cc"
-      ]
+      let examples = ["b", "bb"];
+      let counter_examples = ["c", "bc", "cb", "cc"];
       await g_explore(["a"], rules, 3, async found => {
-        console.log(found)
+        console.log(found);
         if (await m_js_equals_json(found, ["b"])) {
           console.log(found);
-          process.exit()
+          process.exit();
         }
-      })
-    })
+      });
+    });
   });
   return;
   let rules = [{
