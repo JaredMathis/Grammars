4abf5b5cb7b98337dc7f7fd5f55c821650faa19a
JaredMathis <jared.mathis@native.consulting>
2022-07-18 00:38:23 -0400

    main file_js_auto g_letters_to_number

--- src/g/letters/to/number.mjs
+++ src/g/letters/to/number.mjs
@@ -1,3 +1,5 @@
+import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
+import {m_js_for_each} from "./../../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {list_join} from "./../../../../node_modules/mykro/src/list/join.mjs";
 import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {list_index_last} from "./../../../../node_modules/mykro/src/list/index/last.mjs";
@@ -15,7 +17,17 @@ export async function g_letters_to_number(s) {
   let numbers_without_0 = await list_take(numbers_as_list, await list_index_last(numbers_as_list));
   let letters_without_last_10 = await list_take(letters_as_list, await list_size(letters_as_list) - await list_size(numbers_as_list));
   let target = await list_join([numbers_without_0, letters_without_last_10, ["0"]]);
-  console.log(target);
+  let a = {};
+  await m_js_for_each(letters_as_list, (letter, index) => {
+    let other = target[index];
+    a[letter] = other;
+  });
+  let s_as_list = await m_js_string_to_list(s);
+  let mapped = await list_map(s_as_list, letter => {
+    return a[letter];
+  });
+  console.log(mapped);
+  parseInt();
 }
 async function constant_letters_sorted() {
   let letters = await constant_alphabet();
