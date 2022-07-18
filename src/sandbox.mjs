f1b4f0042beee0fb0481c1beeabaa5d74cd0137a
JaredMathis <jared.mathis@native.consulting>
2022-07-18 12:05:14 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -7,20 +7,26 @@ import {list_add} from "./../node_modules/mykro/src/list/add.mjs";
 import {g_rule_apply_at} from "./g/rule/apply/at.mjs";
 import {m_js_arguments_assert} from "./../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {g_rule_apply_get} from "./g/rule/apply/get.mjs";
-import {m_js_for_each} from "mykro/src/m/js/for/each.mjs";
+import {m_js_for_each} from "./../node_modules/mykro/src/m/js/for/each.mjs";
 import {g_letters_to_number} from "./g/letters/to/number.mjs";
 import {g_letters_from_number} from "./g/letters/from/number.mjs";
-import {list_index_of} from "mykro/src/list/index/of.mjs";
-import {list_where} from "mykro/src/list/where.mjs";
-import {list_size} from "mykro/src/list/size.mjs";
+import {list_index_of} from "./../node_modules/mykro/src/list/index/of.mjs";
+import {list_where} from "./../node_modules/mykro/src/list/where.mjs";
+import {list_size} from "./../node_modules/mykro/src/list/size.mjs";
+import {m_js_comment} from "./../node_modules/mykro/src/m/js/comment.mjs";
 export async function sandbox() {
   Error.stackTraceLimit = Infinity;
+  m_js_comment();
   let _case = {
     examples_get: () => [["b"], ["b", "b"]],
-    counter_examples: [["c"], ["b", "c"], ["c", "b"], ["c", "c"]],
+    counter_examples: [["c"], ["b", "c"], ["c", "b"], ["c", "c"]]
+  };
+  _case = {
+    examples_get: () => [["b"], ["b", "c"], ["b", "c"]],
+    counter_examples: [["c"], ["c", "b"], ["c", "c"]]
   };
   await g_models(_case.examples_get, _case.counter_examples, rules => {
-    console.log(rules)
+    console.log(rules);
   });
   return;
   let rules = [{
@@ -46,24 +52,23 @@ async function g_models(examples_get, counter_examples, for_each_model) {
   await g_generate_rules_depth([{
     left: ["a"],
     right: ["a"]
-  }], 2, async (rules) => {
+  }], 2, async rules => {
     let examples = examples_get();
     let counter_example_found = false;
-    await g_explore(["a"], rules, 3, async (found) => {
-      let matches_examples = await list_where(examples, async (e) => await m_js_equals_json(found, e));
-      await m_js_for_each(matches_examples, async (e) => {
+    await g_explore(["a"], rules, 3, async found => {
+      let matches_examples = await list_where(examples, async e => await m_js_equals_json(found, e));
+      await m_js_for_each(matches_examples, async e => {
         await list_remove(examples, e);
       });
-      let matches_counter = await list_where(counter_examples, async (e) => await m_js_equals_json(found, e));
+      let matches_counter = await list_where(counter_examples, async e => await m_js_equals_json(found, e));
       if (await list_size(matches_counter) >= 1) {
         counter_example_found = true;
       }
     });
     if (!counter_example_found) {
-      if ((await list_size(examples)) === 0) {
+      if (await list_size(examples) === 0) {
         for_each_model(rules);
       }
     }
   });
 }
-
