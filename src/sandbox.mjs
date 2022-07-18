08fde75c8621de2a49320129e3e94c2774eef356
JaredMathis <jared.mathis@native.consulting>
2022-07-18 11:08:34 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,3 +1,4 @@
+import {g_generate_rules_depth} from "./g/generate/rules/depth.mjs";
 import {m_js_equals_json} from "./../node_modules/mykro/src/m/js/equals/json.mjs";
 import {g_generate_rules} from "./g/generate/rules.mjs";
 import {g_explore} from "./g/explore.mjs";
@@ -13,31 +14,29 @@ import {list_index_of} from "mykro/src/list/index/of.mjs";
 import {list_where} from "mykro/src/list/where.mjs";
 import {list_size} from "mykro/src/list/size.mjs";
 export async function sandbox() {
-  await g_generate_rules([{
+  await g_generate_rules_depth([{
     left: ["a"],
     right: ["a"]
-  }], async rules => {
-    await g_generate_rules(rules, async rules => {
-      let examples = [["b"], ["b", "b"]];
-      let counter_examples = [["c"], ["b", "c"], ["c", "b"], ["c", "c"]];
-      let counter_example_found = false;
-      await g_explore(["a"], rules, 3, async found => {
-        let matches_examples = await list_where(examples, async e => await m_js_equals_json(found, e));
-        await m_js_for_each(matches_examples, async e => {
-          await list_remove(examples, e);
-        });
-        let matches_counter = await list_where(counter_examples, async e => await m_js_equals_json(found, e));
-        if (await list_size(matches_counter) >= 1) {
-          counter_example_found = true;
-        }
+  }], 2, async rules => {
+    let examples = [["b"], ["b", "b"]];
+    let counter_examples = [["c"], ["b", "c"], ["c", "b"], ["c", "c"]];
+    let counter_example_found = false;
+    await g_explore(["a"], rules, 3, async found => {
+      let matches_examples = await list_where(examples, async e => await m_js_equals_json(found, e));
+      await m_js_for_each(matches_examples, async e => {
+        await list_remove(examples, e);
       });
-      if (!counter_example_found) {
-        if (await list_size(examples) === 0) {
-          console.log(rules);
-          process.exit();
-        }
+      let matches_counter = await list_where(counter_examples, async e => await m_js_equals_json(found, e));
+      if (await list_size(matches_counter) >= 1) {
+        counter_example_found = true;
       }
     });
+    if (!counter_example_found) {
+      if (await list_size(examples) === 0) {
+        console.log(rules);
+        process.exit();
+      }
+    }
   });
   return;
   let rules = [{
