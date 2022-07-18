1d20d102103c8d146edf07180ead57ffa89f20b7
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:33:45 -0400

    main file_js_auto g_letters_from_number

--- src/g/letters/from/number.mjs
+++ src/g/letters/from/number.mjs
@@ -1,5 +1,15 @@
+import {m_js_string_listify} from "./../../../../node_modules/mykro/src/m/js/string/listify.mjs";
+import {g_letters_number_lookups} from "./../number/lookups.mjs";
+import {m_js_number_is} from "./../../../../node_modules/mykro/src/m/js/number/is.mjs";
+import {constant_alphabet} from "mykro/src/constant/alphabet.mjs";
+import {list_size} from "mykro/src/list/size.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_letters_from_number() {
-  await m_js_arguments_assert()(arguments);
+export async function g_letters_from_number(n) {
+  await m_js_arguments_assert(m_js_number_is)(arguments);
   let {to_letter} = await g_letters_number_lookups();
+  let a = n.toString(await list_size(await constant_alphabet()));
+  let result = await m_js_string_listify(a, b => {
+    return to_letter[b];
+  });
+  return result;
 }
