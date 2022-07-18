fb945b533b95f1abfe58470ef6b77a7f7d84e0c0
JaredMathis <jared.mathis@native.consulting>
2022-07-18 00:16:12 -0400

    main file_js_auto g_letters_to_number

--- src/g/letters/to/number.mjs
+++ src/g/letters/to/number.mjs
@@ -1,3 +1,4 @@
+import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {list_index_last} from "./../../../../node_modules/mykro/src/list/index/last.mjs";
 import {list_take} from "./../../../../node_modules/mykro/src/list/take.mjs";
 import {constant_numbers} from "./../../../../node_modules/mykro/src/constant/numbers.mjs";
@@ -11,6 +12,7 @@ export async function g_letters_to_number(s) {
   let numbers = await constant_numbers();
   let numbers_as_list = await m_js_string_to_list(numbers);
   let numbers_without_0 = await list_take(numbers_as_list, await list_index_last(numbers_as_list));
+  let letters_without_last_10 = await list_take(letters_as_list, await list_size(letters_as_list) - await list_size(numbers));
   console.log(numbers_without_0);
   console.log(letters_as_list);
 }
