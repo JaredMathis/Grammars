b69117d9f65ff59fbdaf47dcd7b8a2615a96e7ab
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:43:23 -0400

    main sandbox sandbox

--- src/g/letters/from/number.mjs
+++ src/g/letters/from/number.mjs
@@ -9,7 +9,7 @@ export async function g_letters_from_number(n) {
   await m_js_arguments_assert(m_js_number_is)(arguments);
   let {to_letter} = await g_letters_number_lookups();
   let a = n.toString(await list_size(await constant_alphabet_sorted()));
-  let result = await m_js_string_listify(s, async l => {
+  let result = await m_js_string_listify(a, async l => {
     return await list_map(l, async letter => {
       return to_letter[letter];
     });
