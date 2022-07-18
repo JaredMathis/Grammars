45a46c8ee4be2675d190ab1175475396195b6eed
JaredMathis <jared.mathis@native.consulting>
2022-07-17 20:13:44 -0400

    main npm_install_latest_mykro npmilmy

--- src/g/generate/side.mjs
+++ src/g/generate/side.mjs
@@ -1,4 +1,6 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_generate_side() {
+export async function g_generate_side(rows, for_each_generated) {
   await m_js_arguments_assert()(arguments);
+  let symbol = 1;
+  
 }
