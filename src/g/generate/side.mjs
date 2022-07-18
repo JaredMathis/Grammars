082210c9100d1086652d1298a34bb24e10875340
JaredMathis <jared.mathis@native.consulting>
2022-07-17 20:16:44 -0400

    main file_js_auto g_generate_side

--- src/g/generate/side.mjs
+++ src/g/generate/side.mjs
@@ -1,6 +1,13 @@
+import {m_js_for_range} from "./../../../node_modules/mykro/src/m/js/for/range.mjs";
+import {m_js_function_is} from "./../../../node_modules/mykro/src/m/js/function/is.mjs";
+import {m_js_number_is} from "./../../../node_modules/mykro/src/m/js/number/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_generate_side(rows, for_each_generated) {
-  await m_js_arguments_assert()(arguments);
-  let symbol = 1;
-  
+  await m_js_arguments_assert(m_js_number_is, m_js_function_is)(arguments);
+  let symbol = 0;
+  await m_js_for_range(rows, async row => {
+    let result = {};
+    result.pair = [++symbol + "", ++symbol + ""];
+    for_each_generated(result);
+  });
 }
