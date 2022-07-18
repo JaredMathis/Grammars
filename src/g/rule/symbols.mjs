ea0d7074bba0cdefdd28ca063bdd7494fef5d61b
JaredMathis <jared.mathis@native.consulting>
2022-07-18 14:53:04 -0400

    main file_js_auto g_rule_symbols

--- src/g/rule/symbols.mjs
+++ src/g/rule/symbols.mjs
@@ -1,4 +1,7 @@
+import {list_join} from "./../../../node_modules/mykro/src/list/join.mjs";
+import {m_js_defined_is} from "./../../../node_modules/mykro/src/m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_rule_symbols() {
-  await m_js_arguments_assert()(arguments);
+export async function g_rule_symbols(rule) {
+  await m_js_arguments_assert(m_js_defined_is)(arguments);
+  return await list_join([rule.left, rule.right]);
 }
