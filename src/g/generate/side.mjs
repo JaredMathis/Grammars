543f733c23b293d569230b523fcf63a8f4565553
JaredMathis <jared.mathis@native.consulting>
2022-07-17 20:29:51 -0400

    main file_js_auto g_generate_side

--- src/g/generate/side.mjs
+++ src/g/generate/side.mjs
@@ -1,3 +1,4 @@
+import {m_js_for_each} from "./../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {list_add} from "./../../../node_modules/mykro/src/list/add.mjs";
 import {m_js_for_range} from "./../../../node_modules/mykro/src/m/js/for/range.mjs";
 import {m_js_function_is} from "./../../../node_modules/mykro/src/m/js/function/is.mjs";
@@ -10,8 +11,13 @@ export async function g_generate_side(rows, for_each_generated) {
   await m_js_for_range(rows, async row => {
     let result = {};
     result.pair = [++symbol + "", ++symbol + ""];
+    result.other = [];
     await list_add(results, result);
   });
-  await m_js_for_range(rows * 2, async s => {});
+  await m_js_for_range(rows * 2, async s => {
+    await m_js_for_each(results, async r => {
+      await list_add(r.other, s);
+    });
+  });
   await for_each_generated(results);
 }
