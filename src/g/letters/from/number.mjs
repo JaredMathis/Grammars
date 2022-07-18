c4f30d70fa5123e565bb07af8aef5ee7a0bc5d6d
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:43:01 -0400

    main file_js_auto g_letters_from_number

--- src/g/letters/from/number.mjs
+++ src/g/letters/from/number.mjs
@@ -3,7 +3,6 @@ import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_string_listify} from "./../../../../node_modules/mykro/src/m/js/string/listify.mjs";
 import {g_letters_number_lookups} from "./../number/lookups.mjs";
 import {m_js_number_is} from "./../../../../node_modules/mykro/src/m/js/number/is.mjs";
-import {constant_alphabet} from "mykro/src/constant/alphabet.mjs";
 import {list_size} from "mykro/src/list/size.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_letters_from_number(n) {
